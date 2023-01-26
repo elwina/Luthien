41e5293d7f11107302cfd78b0c4facecbd681502
elwina <elwina@yeah.net>
2023-01-26 23:08:18 +0800

    webui add monaco

--- web/configui/src/App.svelte
+++ web/configui/src/App.svelte
@@ -1,8 +1,39 @@
 <script lang="ts">
-  import Counter from "./lib/Counter.svelte";
+  import jsonWorker from "monaco-editor/esm/vs/language/json/json.worker?worker";
+  import * as monaco from "monaco-editor";
+  import { onMount } from "svelte";
+
+  self.MonacoEnvironment = {
+    getWorker(_: string, label: string) {
+      return new jsonWorker();
+    },
+  };
+
+  onMount(() => {
+    let editor: monaco.editor.IStandaloneCodeEditor;
+
+    editor = monaco.editor.create(
+      document.getElementById("codeEditBox") as HTMLElement,
+      {
+        value: "asd", // 编辑器初始显示文字
+        language: "json",
+        automaticLayout: true, // 自适应布局
+        theme: "vs-dark",
+        foldingStrategy: "indentation",
+        renderLineHighlight: "all",
+        selectOnLineNumbers: true,
+        minimap: {
+          enabled: false,
+        },
+        readOnly: false,
+        fontSize: 16, // 字体大小
+        scrollBeyondLastLine: false, // 取消代码后面一大段空白
+        overviewRulerBorder: false, // 不要滚动条的边框
+      }
+    );
+  });
 </script>
 
 <main>
-  <h1>配置生成器</h1>
-  <div />
+  <div id="codeEditBox" style:height="100vh" style:width="50vw" />
 </main>
