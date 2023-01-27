4e6f6d65b2bffa0cfd9c273e3553fe9f90f956f8
elwina <elwina@yeah.net>
2023-01-27 14:42:09 +0800

    20230127 noon

--- web/information.py
+++ web/information.py
@@ -1,3 +1,4 @@
+from dataclasses import Field
 import json
 import os
 
@@ -6,7 +7,7 @@ def getInf():
     inf = {}
     inf["module"] = []
 
-    from config.register import MODULE_LIST, IO_LIST, RECORDER_LIST
+    from config.register import MODULE_LIST,FIELD_LIST, IO_LIST, RECORDER_LIST
     for (name, Mo) in MODULE_LIST.items():
         m = Mo()
         moInf = {"name": name, "input": m.inInf, "output": m.outInf}
@@ -14,6 +15,7 @@ def getInf():
 
     inf["IO"] = list(IO_LIST.keys())
     inf["Recorder"] = list(RECORDER_LIST.keys())
+    inf["Field"]=list(FIELD_LIST.keys())
 
     jsonpath = "web/configui/src/information.json"
     with open(jsonpath, 'w') as f:
